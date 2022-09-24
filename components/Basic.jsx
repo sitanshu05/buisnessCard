fea94d0c76f4a1b100f98d2712862a2298190c7d
sitanshu05 <01fe20bcs260@kletech.ac.in>
2022-09-24 11:12:29 +0530

    image test

--- components/Basic.jsx
+++ components/Basic.jsx
@@ -5,7 +5,7 @@ import { SiGmail } from "react-icons/si";
 export default function Basic(){
     return(
     <main className="header">
-        <img src="components\Image\profile.jpg" alt="Profile Picture" className="Profile"/>
+        <img src="https://github.com/sitanshu05/buisnessCard/blob/0707a2fc2ab90bc80acdd23170feee21557b37b7/components/Image/profile.jpg" alt="Profile Picture" className="Profile"/>
         <h1 className="name">Sitanshu Hallad</h1>
         <h1 className="job">Frontend Developer</h1>
         <a href="" className="website">sitanshu.web</a>
